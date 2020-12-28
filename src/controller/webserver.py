c2d57254f1a6336df121cb87f56bc03c4f2d3131
Giovanni <giovanni.bartolomeo.rg@gmail.com>
2020-12-28 19:39:46 +0100

    implemented image upload UI

--- src/controller/webserver.py
+++ src/controller/webserver.py
@@ -1,9 +1,44 @@
-from flask import Blueprint, request, jsonify, render_template
+from flask import Blueprint, request, jsonify, render_template, redirect, url_for
+from src.tools.image_upload import direct_image_upload
 
 webserver = Blueprint('webserver', __name__)
 
 
 @webserver.route('/', methods=['GET'])
 def index_page():
-
     return render_template('index.html')
+
+
+@webserver.route('/add', methods=['GET'])
+def add_page():
+    return render_template('add.html')
+
+
+@webserver.route('/add', methods=['POST'])
+def add_face():
+    if 'file' not in request.files:
+        return "invalid file", 200
+    file = request.files['file']
+    try:
+        direct_image_upload(
+            file,
+            request.form['name'].strip(),
+            request.form['surname'].strip(),
+            request.form['id'].strip()
+        )
+    except Exception as e:
+        print(e)
+        return "upload failed, is the upload service available?", 200
+
+    return redirect(url_for('webserver.index_page'))
+
+
+@webserver.route('/login', methods=['POST'])
+def login():
+    """
+        Mock method for login. Does not generate any token, only for demonstrative purpose.
+    """
+    if request.form['id'] == "admin" and request.form['password'] == "admin":
+        return redirect(url_for('webserver.add_page'))
+    else:
+        return "Invalid credentials", 200
---
+++ src/tools/image_upload.py
@@ -0,0 +1,17 @@
+import base64
+from src.config import UPLOAD_URL
+import requests
+
+
+def direct_image_upload(file, name, surname, id):
+    imagebase64 = base64.b64encode(file.read()).decode('utf-8')
+    request_body = {
+        "name": str(name),
+        "surname": str(surname),
+        "img_base64": imagebase64
+    }
+
+    response = requests.post(UPLOAD_URL + "/api/store/" + id, json=request_body)
+
+    if response.status_code is not 200:
+        raise Exception("Upload failed")
